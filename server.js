4a2f10f0637eacab2487ae9b37d25c7a9d17886a
vishnu vp <vishnuprakaash257@gmail.com>
2023-10-15 19:22:21 +0530

    basic route

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app');
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`app listening on ${port}`);
